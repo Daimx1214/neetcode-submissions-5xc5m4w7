2afc09bada2bae9f78729cc680359ce580178ef9
Daim ALi <daimx1214@gmail.com>
2026-04-15 23:13:48 +0500

    Add: valid-parenthesis-string - submission-0

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-0.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int balance = 0;
+        for(char c: s){
+            if(c =='(' || c == '*'){
+                balance++;
+            }
+            else{
+                balance--;
+            }
+            if(balance<0){
+                return false;
+            }
+        }
+        balance = 0;
+        for(int i=s.size()-1; i>=0; i--){
+            char c = s[i];
+            if(c ==')' || c == '*'){
+                balance++;
+            }
+            else{
+                balance--;
+            }
+            if(balance<0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
